74ee4f35e3758bfc9168b17e9d8e4e94032643ca
Alan Halatian <alanhalatian@gmail.com>
2026-07-25 21:27:44 -0300

    Build topologies from a parsed mmCIF file in main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 use std::fs;
 
-use prote::Cif;
+use prote::{mmcif::MmCIF, Cif};
 
 fn main() {
-    let cif_file = fs::read_to_string("4d1m.cif").unwrap();
+    let cif_file = fs::read_to_string("./cifs/4d1m.cif").unwrap();
     let cif = Cif::new(&cif_file);
-    let block = cif.block("4D1M").unwrap();
-    println!("{:#?}", block.get("_entry.id"));
-    // for x in block.column("_atom_site.Cartn_x").unwrap() {
-    //     // all x-coords, no intermediate Vec
-    //     println!("{:#?}", x);
-    // }
+    let mmcif = MmCIF::new(cif).unwrap();
+    println!("{:#?}", mmcif.build_topologies());
 }
